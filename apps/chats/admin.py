627704028e0ec5cf23b8bc25140547c51a67993e
Toktorov <ktoktorov2005@gmail.com>
2023-01-06 00:45:52 +0600

    chat model and customazing admin panel

--- apps/chats/admin.py
+++ apps/chats/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from apps.chats.models import Chat, Message
 
 # Register your models here.
+class ChatAdmin(admin.ModelAdmin):
+    list_filter = ('id', )
+    list_display = ('from_chat_user', 'to_chat_user', 'created')
+    search_fields = ('from_chat_user', 'to_chat_user', 'created')
+    list_per_page = 20
+
+class MessageAdmin(admin.ModelAdmin):
+    list_filter = ('id', )
+    list_display = ('chat', 'from_user', 'to_user', 'is_read', 'created')
+    search_fields = ('chat', 'from_user', 'to_user', 'is_read', 'created')
+    list_per_page = 20
+
+admin.site.register(Chat, ChatAdmin)
+admin.site.register(Message, MessageAdmin)
--- apps/chats/models.py
+++ apps/chats/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from apps.users.models import User
 
 # Create your models here.
+class Chat(models.Model):
+    from_chat_user = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE,
+        related_name = "from_user_chat",
+        verbose_name = "Чат от пользователя"
+    )
+    to_chat_user = models.ForeignKey(
+        User, 
+        on_delete = models.CASCADE,
+        related_name = "to_user_chat",
+        verbose_name = "Чат к пользователю"
+    )
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return f"{self.id} -- {self.from_chat_user} -- {self.to_chat_user} -- {self.created}"
+
+    class Meta:
+        verbose_name = "Чат"
+        verbose_name_plural = "Чаты"
+
+class Message(models.Model):
+    chat = models.ForeignKey(
+        Chat, 
+        on_delete = models.CASCADE,
+        related_name = 'user_chat_id',
+        verbose_name = "ID сообщения"
+    )
+    from_user = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE,
+        related_name = "chat_from_user",
+        verbose_name = "Сообщение от пользователя"
+    )
+    to_user = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE,
+        related_name = "chat_to_user",
+        verbose_name = "Сообщение к пользователю"
+    )
+    message = models.CharField(
+        max_length = 255,
+        verbose_name = "Сообщение"
+    )
+    is_read = models.BooleanField(
+        default=False,
+        verbose_name="Прочитан"
+    )
+    created = models.TimeField(
+        auto_now_add = True
+    )
+
+    def __str__(self):
+        return f"{self.chat}"
+
+    class Meta:
+        verbose_name = "Сообщение в чате"
+        verbose_name_plural = "Сообщение в чатах"
