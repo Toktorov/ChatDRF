bc221cbbf639022fbafe750975446bee50b82f92
Toktorov <ktoktorov2005@gmail.com>
2023-01-08 21:22:51 +0600

    chat retrieve permissions

--- apps/chats/views.py
+++ apps/chats/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from apps.chats.models import Chat, Message
 from apps.chats.serializers import ChatSerializer, ChatChatRetrieveSerializer
+from apps.chats.permissions import ChatPermissions
 from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAdminUser
 
 # Create your views here.
 class ChatAPIView(ListCreateAPIView):
     queryset = Chat.objects.all()
     serializer_class = ChatSerializer
+    permission_classes = (IsAdminUser, )
 
-    # def perform_create(self, serializer):
-    #     return serializer.save(from_chat_user = self.request.user)
+    def perform_create(self, serializer):
+        return serializer.save(from_chat_user = self.request.user)
 
 class ChatRetrieveAPIView(RetrieveAPIView):
     queryset = Chat.objects.all()
     serializer_class = ChatChatRetrieveSerializer
+    permission_classes = (ChatPermissions, )
