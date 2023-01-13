1999eba784f86a96b5694c0cf332cb3d4b3cc445
Toktorov <ktoktorov2005@gmail.com>
2023-01-13 22:56:00 +0600

    is_read true when request user enters the chat and reads the message of the interlocutor

--- apps/chats/views.py
+++ apps/chats/views.py
@@ -4,6 +4,7 @@ from apps.chats.permissions import ChatPermissions
 from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, CreateAPIView, DestroyAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 from django.db.models import Q
+from rest_framework.response import Response
 
 # Create your views here.
 class ChatAPIView(ListCreateAPIView):
@@ -21,6 +22,15 @@ class ChatRetrieveAPIView(RetrieveAPIView):
     queryset = Chat.objects.all()
     serializer_class = ChatChatRetrieveSerializer
     permission_classes = (ChatPermissions, )
+    
+    def get(self, request, pk):
+        chat = Chat.objects.get(pk = pk)
+        message = Message.objects.filter(chat = chat.id, to_user = request.user)
+        for mes in message:
+            mes.is_read = True 
+            mes.save()
+        serializer = ChatChatRetrieveSerializer(chat)
+        return Response({'message' : serializer.data} )
 
 class ChatDestroyAPIView(DestroyAPIView):
     queryset = Chat.objects.all()
