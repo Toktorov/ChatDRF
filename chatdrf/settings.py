8f73b59504bbb44ed0cf85eb45bbfaf071b70488
Toktorov <ktoktorov2005@gmail.com>
2023-01-13 17:09:48 +0600

    .env file and update requirements.txt

--- chatdrf/settings.py
+++ chatdrf/settings.py
@@ -10,24 +10,24 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from datetime import timedelta
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7e2q6a!#0=!#p%!#-@#*5c*f!f@m0t&s-qh52-cbhbydrh%cjb'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = ["*"]
-
+ALLOWED_HOSTS = list(config("ALLOWED_HOSTS"))
 
 # Application definition
 
